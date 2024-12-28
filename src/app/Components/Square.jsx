f386ec554791398e6d3ae96335e6b2375742b672
WesbertJr <wesbertjr@gmail.com>
2024-12-27 22:44:58 -0500

    dynamic X and O based on randomNum

--- src/app/Components/Square.jsx
+++ src/app/Components/Square.jsx
@@ -1,6 +1,19 @@
+"use client";
+import React, { useState } from 'react';
+
 export default function Square() {
+  const [player, setPlayer] = useState();
+  function handleClick() {
+    let randomNumber = Math.round(Math.random());
+    let position = randomNumber ? "O" : "X";
+    setPlayer(position);
+    }
+  
     return (
-        <div className="w-20 h-20 border-black border">
+        <div 
+            className="w-20 h-20 border-black border" 
+             onClick={handleClick}>
+            <p className="text-6xl text-center pt-3">{player}</p>
         </div>
     );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,10 @@
+"use client";
 import Image from "next/image";
 import Square from "./Components/Square";
+import React, { useState } from 'react';
+
 export default function Home() {
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-5 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h3 className="text-3xl font-bold underline center ">Tic Tac Toe</h3>
@@ -14,7 +18,6 @@ export default function Home() {
       <Square/>
       <Square/>
       <Square/>
-
       </span>
       <Image src={"/next.svg"} alt="Vercel Logo" width={200} height={200} />  
     </div>
