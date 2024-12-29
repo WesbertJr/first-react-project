5c71ecde14d9c6068cfd121b9d73945af9c4adbf
WesbertJr <wesbertjr@gmail.com>
2024-12-29 00:04:23 -0500

    Fixed issue where player can overwrite previous square

--- src/app/Components/Square.jsx
+++ src/app/Components/Square.jsx
@@ -7,16 +7,23 @@ export default function Square({index}) {
   function handleClick() {
         let randomNumber = Math.round(Math.random());
         let position = randomNumber ? "O" : "X";
-        setPlayer(position);
-        setInput(prevInput => [...prevInput, index]);
+        
+        setInput((prevInput) => {
+          if (!prevInput.includes(index)) {
+            setPlayer(position);
+            return [...prevInput, index];;
+          } else {
+            return prevInput;
+          }
+        });
 }
   
     return (
         <div 
             className="w-20 h-20 border-black border" 
              onClick={handleClick}>
-            {/* <p className="text-6xl text-center pt-3">{player}</p> */}
-            <p className="text-6xl text-center pt-3">{input}</p>
+            <p className="text-6xl text-center pt-3">{player}</p>
+            {/* <p className="text-6xl text-center pt-3">{input}</p> */}
         </div>
     );
 }
