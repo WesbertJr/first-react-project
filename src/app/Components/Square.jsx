5bcfd55dac370a8d45b7be1b38e78170f3c0d90d
WesbertJr <wesbertjr@gmail.com>
2024-12-28 23:57:38 -0500

    dynamically passing previous index

--- src/app/Components/Square.jsx
+++ src/app/Components/Square.jsx
@@ -1,19 +1,22 @@
 "use client";
 import React, { useState } from 'react';
 
-export default function Square() {
+export default function Square({index}) {
   const [player, setPlayer] = useState();
+  const [input, setInput] = useState([]);
   function handleClick() {
-    let randomNumber = Math.round(Math.random());
-    let position = randomNumber ? "O" : "X";
-    setPlayer(position);
-    }
+        let randomNumber = Math.round(Math.random());
+        let position = randomNumber ? "O" : "X";
+        setPlayer(position);
+        setInput(prevInput => [...prevInput, index]);
+}
   
     return (
         <div 
             className="w-20 h-20 border-black border" 
              onClick={handleClick}>
-            <p className="text-6xl text-center pt-3">{player}</p>
+            {/* <p className="text-6xl text-center pt-3">{player}</p> */}
+            <p className="text-6xl text-center pt-3">{input}</p>
         </div>
     );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,15 +9,15 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-5 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h3 className="text-3xl font-bold underline center ">Tic Tac Toe</h3>
       <span className="grid grid-cols-3 gap-0.5">
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
+      <Square index={0}/>
+      <Square index={1}/>
+      <Square index={2}/>
+      <Square index={3}/>
+      <Square index={4}/>
+      <Square index={5}/>
+      <Square index={6}/>
+      <Square index={7}/>
+      <Square index={8}/>
       </span>
       <Image src={"/next.svg"} alt="Vercel Logo" width={200} height={200} />  
     </div>
